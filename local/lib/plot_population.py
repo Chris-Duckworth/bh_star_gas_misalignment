9ac01f36057e8164f961eb5b93c3ca3bd0d5a7e3
Chris-Duckworth <cd201@st-andrews.ac.uk>
2019-10-19 00:13:19 -0400

    added new notebook to look at all bh properties. need to add energy injection and anisotropy.

--- local/lib/plot_population.py
+++ local/lib/plot_population.py
@@ -5,22 +5,74 @@ plot_population - functions to plot different populations in the manga like tng 
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats
+import split_population
 
-
-def plot_property_evolution(branch_z, property, ax, label=None, color='k', linestyle='solid'):
+def plot_property_evolution(time, property, ax, label=None, color='k', linestyle='solid'):
     '''
     Given a population of galaxies with a defined property and equivalent redshift, this 
     finds the average value at every snapshot and finds the median and standard error.
     These are then plotted. 
     '''
-    ts = np.unique(branch_z) 
+    ts = np.unique(time) 
     # finding average value for each defined z.
     av = np.zeros(ts.shape[0])
     std = np.zeros(ts.shape[0])
     for i, z in enumerate(ts):
-        av[i] = np.median(property[branch_z == z])
-        std[i] = scipy.stats.sem(property[branch_z == z]) 
+        av[i] = np.mean(property[time == z])
+        std[i] = scipy.stats.sem(property[time == z]) 
     ax.plot(ts, av, linestyle=linestyle, color=color, label=label)
     ax.fill_between(ts, av - std, av + std, alpha=0.2, color=color)
     return
-    
+
+
+def plot_row_evolution(QU, GV, SF, mass_tab, property, condition, ax):
+	'''
+	Given a property (and a minimum value cut on the property), this returns a full split
+	on both morphology and percentiles of mass. The axes object supplied should be 1 x 3.
+	'''
+	# Splitting table data.
+	QU_tab_HM, QU_align_tab_HM, QU_mis_tab_HM, QU_tab_LM, QU_align_tab_LM, QU_mis_tab_LM = split_population.combine_with_tree_split_on_pa_and_mass(QU, mass_tab, lower_PA=30, upper_PA=30, lower_percentile=33, upper_percentile=66)
+	_, _, QU_counter_tab_HM, _, _, QU_counter_tab_LM = split_population.combine_with_tree_split_on_pa_and_mass(QU, mass_tab, lower_PA=30, upper_PA=150, lower_percentile=33, upper_percentile=66)
+	
+	GV_tab_HM, GV_align_tab_HM, GV_mis_tab_HM, GV_tab_LM, GV_align_tab_LM, GV_mis_tab_LM = split_population.combine_with_tree_split_on_pa_and_mass(GV, mass_tab, lower_PA=30, upper_PA=30, lower_percentile=33, upper_percentile=66)
+	_, _, GV_counter_tab_HM, _, _, GV_counter_tab_LM = split_population.combine_with_tree_split_on_pa_and_mass(GV, mass_tab, lower_PA=30, upper_PA=150, lower_percentile=33, upper_percentile=66)
+	
+	SF_tab_HM, SF_align_tab_HM, SF_mis_tab_HM, SF_tab_LM, SF_align_tab_LM, SF_mis_tab_LM = split_population.combine_with_tree_split_on_pa_and_mass(SF, mass_tab, lower_PA=30, upper_PA=30, lower_percentile=33, upper_percentile=66)  
+	_, _, SF_counter_tab_HM, _, _, SF_counter_tab_LM = split_population.combine_with_tree_split_on_pa_and_mass(SF, mass_tab, lower_PA=30, upper_PA=150, lower_percentile=33, upper_percentile=66)  
+	# Quenched.
+	# Top 33% $M_{stel}$
+	plot_property_evolution(QU_tab_HM.branch_lookback_time.values[QU_tab_HM[property].values > condition], QU_tab_HM[property].values[QU_tab_HM[property].values > condition], ax[0], r'Top 33% $M_{stel}$', color='salmon')
+	plot_property_evolution(QU_align_tab_HM.branch_lookback_time.values[QU_align_tab_HM[property].values > condition], QU_align_tab_HM[property].values[QU_align_tab_HM[property].values > condition], ax[0], r'Top 33\% $M_{stel}$, $\Delta$PA $< 30$', color='salmon', linestyle='dotted') 
+	plot_property_evolution(QU_mis_tab_HM.branch_lookback_time.values[QU_mis_tab_HM[property].values > condition], QU_mis_tab_HM[property].values[QU_mis_tab_HM[property].values > condition], ax[0], r'Top 33\% $M_{stel}$, $\Delta$PA $> 30$',color='salmon', linestyle='dashed') 
+	plot_property_evolution(QU_counter_tab_HM.branch_lookback_time.values[QU_counter_tab_HM[property].values > condition], QU_counter_tab_HM[property].values[QU_counter_tab_HM[property].values > condition], ax[0], r'Top 33\% $M_{stel}$, $\Delta$PA $> 150$',color='salmon', linestyle='-.') 
+	# Bottom 33% $M_{stel}$
+	plot_property_evolution(QU_tab_LM.branch_lookback_time.values[QU_tab_LM[property].values > condition], QU_tab_LM[property].values[QU_tab_LM[property].values > condition], ax[0], r'Bottom 33\% $M_{stel}$', color='steelblue')
+	plot_property_evolution(QU_align_tab_LM.branch_lookback_time.values[QU_align_tab_LM[property].values > condition], QU_align_tab_LM[property].values[QU_align_tab_LM[property].values > condition], ax[0], r'Bottom 33\% $M_{stel}$, $\Delta$PA $< 30$', color='steelblue', linestyle='dotted') 
+	plot_property_evolution(QU_mis_tab_LM.branch_lookback_time.values[QU_mis_tab_LM[property].values > condition], QU_mis_tab_LM[property].values[QU_mis_tab_LM[property].values > condition], ax[0], r'Bottom 33\% $M_{stel}$, $\Delta$PA $> 30$',color='steelblue', linestyle='dashed') 
+	plot_property_evolution(QU_counter_tab_LM.branch_lookback_time.values[QU_counter_tab_LM[property].values > condition], QU_counter_tab_LM[property].values[QU_counter_tab_LM[property].values > condition], ax[0], r'Bottom 33\% $M_{stel}$, $\Delta$PA $> 150$',color='steelblue', linestyle='-.') 
+
+	ax[0].set_xlim([0, 7.93])
+	ax[0].invert_xaxis()
+	# Green valley.
+	# Top 33% $M_{stel}$
+	plot_property_evolution(GV_tab_HM.branch_lookback_time.values[GV_tab_HM[property].values > condition], GV_tab_HM[property].values[GV_tab_HM[property].values > condition], ax[1], r'Top 33% $M_{stel}$', color='salmon')
+	plot_property_evolution(GV_align_tab_HM.branch_lookback_time.values[GV_align_tab_HM[property].values > condition], GV_align_tab_HM[property].values[GV_align_tab_HM[property].values > condition], ax[1], color='salmon', linestyle='dotted') 
+	plot_property_evolution(GV_mis_tab_HM.branch_lookback_time.values[GV_mis_tab_HM[property].values > condition], GV_mis_tab_HM[property].values[GV_mis_tab_HM[property].values > condition], ax[1], color='salmon', linestyle='dashed') 
+	plot_property_evolution(GV_counter_tab_HM.branch_lookback_time.values[GV_counter_tab_HM[property].values > condition], GV_counter_tab_HM[property].values[GV_counter_tab_HM[property].values > condition], ax[1], r'Bottom 33% $M_{stel}$, $\Delta$PA > 150',color='salmon', linestyle='-.') 
+	# Bottom 33% $M_{stel}$
+	plot_property_evolution(GV_tab_LM.branch_lookback_time.values[GV_tab_LM[property].values > condition], GV_tab_LM[property].values[GV_tab_LM[property].values > condition], ax[1], r'Bottom 33% $M_{stel}$', color='steelblue')
+	plot_property_evolution(GV_align_tab_LM.branch_lookback_time.values[GV_align_tab_LM[property].values > condition], GV_align_tab_LM[property].values[GV_align_tab_LM[property].values > condition], ax[1], r'Bottom 33% $M_{stel}$, $\Delta$PA $< 30$', color='steelblue', linestyle='dotted') 
+	plot_property_evolution(GV_mis_tab_LM.branch_lookback_time.values[GV_mis_tab_LM[property].values > condition], GV_mis_tab_LM[property].values[GV_mis_tab_LM[property].values > condition], ax[1], 'Bottom 33% $M_{stel}$, $\Delta$PA $> 30$',color='steelblue', linestyle='dashed') 
+	plot_property_evolution(GV_counter_tab_LM.branch_lookback_time.values[GV_counter_tab_LM[property].values > condition], GV_counter_tab_LM[property].values[GV_counter_tab_LM[property].values > condition], ax[1], r'Bottom 33% $M_{stel}$, $\Delta$PA $> 150$',color='steelblue', linestyle='-.') 
+	# Star forming.
+	# Top 33% $M_{stel}$
+	plot_property_evolution(SF_tab_HM.branch_lookback_time.values[SF_tab_HM[property].values > condition], SF_tab_HM[property].values[SF_tab_HM[property].values > condition], ax[2], r'Top 33% $M_{stel}$', color='salmon')
+	plot_property_evolution(SF_align_tab_HM.branch_lookback_time.values[SF_align_tab_HM[property].values > condition], SF_align_tab_HM[property].values[SF_align_tab_HM[property].values > condition], ax[2], color='salmon', linestyle='dotted') 
+	plot_property_evolution(SF_mis_tab_HM.branch_lookback_time.values[SF_mis_tab_HM[property].values > condition], SF_mis_tab_HM[property].values[SF_mis_tab_HM[property].values > condition], ax[2], color='salmon', linestyle='dashed') 
+	plot_property_evolution(SF_counter_tab_HM.branch_lookback_time.values[SF_counter_tab_HM[property].values > condition], SF_counter_tab_HM[property].values[SF_counter_tab_HM[property].values > condition], ax[2], r'Top 33\% $M_{stel}$, $\Delta$PA > 150',color='salmon', linestyle='-.') 
+	# Bottom 33% $M_{stel}$
+	plot_property_evolution(SF_tab_LM.branch_lookback_time.values[SF_tab_LM[property].values > condition], SF_tab_LM[property].values[SF_tab_LM[property].values > condition], ax[2], r'Bottom 50\% $M_{stel}$', color='steelblue')
+	plot_property_evolution(SF_align_tab_LM.branch_lookback_time.values[SF_align_tab_LM[property].values > condition], SF_align_tab_LM[property].values[SF_align_tab_LM[property].values > condition], ax[2], color='steelblue', linestyle='dotted') 
+	plot_property_evolution(SF_mis_tab_LM.branch_lookback_time.values[SF_mis_tab_LM[property].values > condition], SF_mis_tab_LM[property].values[SF_mis_tab_LM[property].values > condition], ax[2], color='steelblue', linestyle='dashed') 
+	plot_property_evolution(SF_counter_tab_LM.branch_lookback_time.values[SF_counter_tab_LM[property].values > condition], SF_counter_tab_LM[property].values[SF_counter_tab_LM[property].values > condition], ax[2], r'Bottom 33\% $M_{stel}$, $\Delta$PA > 150',color='steelblue', linestyle='-.') 
+	return 
--- local/lib/split_population.py
+++ local/lib/split_population.py
@@ -5,6 +5,7 @@ split_populations - functions to split the tng100 manga like sample.
 import numpy as np
 import pandas as pd
 
+
 def tng100_pa_sample(tng100_main):
     '''
     For the matched manga-like TNG100 sample, this returns the pa defined. Since our TNG100 
@@ -25,20 +26,22 @@ def SFMS_breakdown(tab):
     return QU, SF, GV
 
 
-def combine_with_tree_split_on_pa(tab, tree_tab, PA=30):
+def combine_with_tree_split_on_pa(tab, tree_tab, lower_PA=30, upper_PA=30):
     '''
     Function that takes a defined sample in the TNG100 - MPL-8 matched population, combines
     with supplementary tree info (for the main branch) and then splits on PA. Returns 3 
     tables; total info, aligned and misaligned
     '''
     all_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values)]
-    align_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[tab.pa_offset.values < PA])]
-    mis_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[tab.pa_offset.values >= PA])]
+    align_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[tab.pa_offset.values < lower_PA])]
+    mis_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[tab.pa_offset.values >= upper_PA])]
     
+    print('All:'+str(all_tab.shape[0] / 50)+' Aligned:'+str(align_tab.shape[0] /50)+' Misaligned:'+str(mis_tab.shape[0] /50))
+
     return all_tab, align_tab, mis_tab
 
 
-def combine_with_tree_split_on_pa_and_group(tab, tree_tab, PA=30):
+def combine_with_tree_split_on_pa_and_group(tab, tree_tab, lower_PA=30, upper_PA=30):
     '''
     Function that takes a defined sample in the TNG100 - MPL-8 matched population, combines
     with supplementary tree info (for the main branch) and then splits on both PA and group
@@ -46,17 +49,21 @@ def combine_with_tree_split_on_pa_and_group(tab, tree_tab, PA=30):
     for centrals and then satellites.
     '''
     cen_all_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[tab.central_flag.values == 1])]
-    cen_align_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[(tab.pa_offset.values < PA) & (tab.central_flag.values == 1)])]
-    cen_mis_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[(tab.pa_offset.values >= PA) & (tab.central_flag.values == 1)] )]
+    cen_align_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[(tab.pa_offset.values < lower_PA) & (tab.central_flag.values == 1)])]
+    cen_mis_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[(tab.pa_offset.values >= upper_PA) & (tab.central_flag.values == 1)] )]
+    
+    print('Centrals. All:'+str(cen_all_tab.shape[0] / 50)+' Aligned:'+str(cen_align_tab.shape[0] /50)+' Misaligned:'+str(cen_mis_tab.shape[0] /50))
     
     sat_all_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[tab.central_flag.values == 0])]
-    sat_align_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[(tab.pa_offset.values < PA) & (tab.central_flag.values == 0)])]
-    sat_mis_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[(tab.pa_offset.values >= PA) & (tab.central_flag.values == 0)] )]
+    sat_align_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[(tab.pa_offset.values < lower_PA) & (tab.central_flag.values == 0)])]
+    sat_mis_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[(tab.pa_offset.values >= upper_PA) & (tab.central_flag.values == 0)] )]
     
+    print('Satellites. All:'+str(sat_all_tab.shape[0] / 50)+' Aligned:'+str(sat_align_tab.shape[0] /50)+' Misaligned:'+str(sat_mis_tab.shape[0] /50))
+	
     return cen_all_tab, cen_align_tab, cen_mis_tab, sat_all_tab, sat_align_tab, sat_mis_tab
 
 
-def combine_with_tree_split_on_pa_and_mass(tab, tree_tab, PA=30):
+def combine_with_tree_split_on_pa_and_mass(tab, tree_tab, lower_PA=30, upper_PA=30, lower_percentile=25, upper_percentile=75, verbose=False):
     '''
     Function that takes a defined sample in the TNG100 - MPL-8 matched population, combines
     with supplementary tree info (for the main branch) and then splits on both PA and mass 
@@ -64,17 +71,20 @@ def combine_with_tree_split_on_pa_and_mass(tab, tree_tab, PA=30):
     By default the top and bottom quartile are returned.
     Returns 6 tables; total info, aligned and misaligned for high mass and low mass.
     ''' 
-    lower_mass = np.percentile(tab.stel_mass.values, 33)
-    upper_mass = np.percentile(tab.stel_mass.values, 66)
-
-    
+    lower_mass = np.percentile(tab.stel_mass.values, lower_percentile)
+    upper_mass = np.percentile(tab.stel_mass.values, upper_percentile)
+	
     high_mass_all_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[tab.stel_mass.values > upper_mass])]
-    high_mass_align_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[(tab.pa_offset.values < PA) & (tab.stel_mass.values > upper_mass)])]
-    high_mass_mis_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[(tab.pa_offset.values >= PA) & (tab.stel_mass.values > upper_mass)] )]
-    
+    high_mass_align_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[(tab.pa_offset.values < lower_PA) & (tab.stel_mass.values > upper_mass)])]
+    high_mass_mis_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[(tab.pa_offset.values >= upper_PA) & (tab.stel_mass.values > upper_mass)] )]
+        
     low_mass_all_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[tab.stel_mass.values <= lower_mass])]
-    low_mass_align_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[(tab.pa_offset.values < PA) & (tab.stel_mass.values <= lower_mass)])]
-    low_mass_mis_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[(tab.pa_offset.values >= PA) & (tab.stel_mass.values <= lower_mass)] )]
+    low_mass_align_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[(tab.pa_offset.values < lower_PA) & (tab.stel_mass.values <= lower_mass)])]
+    low_mass_mis_tab = tree_tab[tree_tab.root_subfind.isin(tab.subfind_id.values[(tab.pa_offset.values >= upper_PA) & (tab.stel_mass.values <= lower_mass)] )]
+    
+    if verbose == True:
+        print('High mass. All:'+str(high_mass_all_tab.shape[0] / 50)+' Aligned:'+str(high_mass_align_tab.shape[0] /50)+' Misaligned:'+str(high_mass_mis_tab.shape[0] /50))
+        print('Low mass.  All:'+str(low_mass_all_tab.shape[0] /50)+' Aligned:'+str(low_mass_align_tab.shape[0] / 50)+' Misaligned:'+str(low_mass_mis_tab.shape[0] / 50))
     
     return high_mass_all_tab, high_mass_align_tab, high_mass_mis_tab, low_mass_all_tab, low_mass_align_tab, low_mass_mis_tab
 
